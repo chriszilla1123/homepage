b784652b8112015172fbe20429eb32666531c5c5
chriszilla1123 <chriszilla1123@gmail.com>
2017-10-02 13:55:04 -0700

    More iframe work, inline positioning, almost finished

--- JS/script.js
+++ JS/script.js
@@ -16,16 +16,46 @@ function getTime(){
     }
     return fixedTime;
   }
-  console.log(h + ":" + m);
   $("#clock").html(h + ":" + m);
 }
 
 $(document).ready(function(){
+  //Set Links
+  var link1 = "https://www.chilltec.net/ampache/";
+  var link2 = "http://www.chilltec.net:8112/";
+  var link3 = "http://www.chilltec.net:61208/";
+  var link4 = "http://www.plex.tv/web";
+  var link5;
+  var link6;
+  var link7;
+  var link8;
   setInterval(getTime, 1000)
   $("#link1").click(function(){
-    $("#window").attr("src", "https://www.chilltec.net/ampache/");
+    $("#window1").attr("src", link1);
+    $("#window1").css("zIndex", 10)
+    $("#window2").css("zIndex", 1)
+    $("#window3").css("zIndex", 1)
+    //$("#window4").css("zIndex", 1)
+  });
+  $("#link2").click(function(){
+    $("#window2").attr("src", link2);
+    $("#window1").css("zIndex", 1)
+    $("#window2").css("zIndex", 10)
+    $("#window3").css("zIndex", 1)
+    //$("#window4").css("zIndex", 1)
   });
   $("#link3").click(function(){
-    $("#window").attr("src", "http://www.chilltec.net:61208/");
+    $("#window3").attr("src", link3);
+    $("#window1").css("zIndex", 1)
+    $("#window2").css("zIndex", 1)
+    $("#window3").css("zIndex", 10)
+    //$("#window4").css("zIndex", 1)
+  });
+  $("#link4").click(function(){
+    //$("#window4").attr("src", link4);
+    //$("#window1").css("zIndex", 1)
+    //$("#window2").css("zIndex", 1)
+    //$("#window3").css("zIndex", 1)
+    //$("#window4").css("zIndex", 10)
   });
 });
